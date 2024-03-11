849c38bce1256733519a46d51328623d6e154079
Konstnt <kosyurch@gmail.com>
2024-03-11 22:37:43 +0300

    Synergy practice report. Case 5 Add fixed ratings

--- Practice_case5.py
+++ Practice_case5.py
@@ -10,7 +10,14 @@ import random
 import os
 import pickle
 
+#Configs
+number_to_guess = random.randint(1, 100) #диапазон чисел для загадывания
+max_attempts = 10                        #макс кол-во попыток игрока 
+
 leaderboard_file = "u_ratings/leaderboard.pickle"
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def show_leaderboard():
     try:
@@ -21,8 +28,8 @@ def show_leaderboard():
             
             print("\n***** ТАБЛИЦА ЛИДЕРОВ *****")
             
-            for idx, entry in enumerate(sorted_leaderboard, start=1):
-                print(f"{idx}. Игрок: {entry['username']}, Попыток: {entry['score']}")
+            for indx, entry in enumerate(sorted_leaderboard, start=1):
+                print(f"{indx}. Игрок: {entry['username']}, Попыток: {entry['score']}")
             
             print("***************************\n")
 
@@ -38,53 +45,81 @@ def update_leaderboard(username, score):
         leaderboard = []
 
     new_entry = {"username": username, "score": score}
-    leaderboard.append(new_entry)
 
     # Сортировка лидеров по наименьшему количеству попыток
+    leaderboard.append(new_entry)
     leaderboard = sorted(leaderboard, key=lambda x: x["score"])
 
-    # Сериализация данных таблицы лидеров обновленной версии в файл leaderboard.pickle
+    # Ограничение таблицы лидеров 10 строками
+    if len(leaderboard) > 10:
+        leaderboard = leaderboard[:10]
+
+    # Сериализация данных обновленной таблицы лидеров в файл leaderboard.pickle
     with open(leaderboard_file, "wb") as file:
         pickle.dump(leaderboard, file)
 
-    print("Таблица лидеров была успешно обновлена!")
+    print("Ваш результат добавлен в таблицу лидеров!")
 
 
 def game_menu():
-    print("Добро пожаловать в игру 'Угадай число'!")
+
     while True:
-        print("N - начать новую игру")
+        print("Добро пожаловать в игру 'Угадай число'!")
+        print("N - Новая игра")
         print("R - показать рейтинг")
         print("X - выход")
         choice = input("Выберите действие: ").upper()
 
         if choice == "N":
-            player_name = input("Введите ваше имя: ")
+            player_name = input("\nВведите ваше имя: ")
             play_game(player_name)
         elif choice == "R":
+            clear_screen()
             show_leaderboard()
         elif choice == "X":
             print("Спасибо за игру! До свидания!")
             break
         else:
-            print("---Опция не выбрана---")
+            clear_screen()
 
 def play_game(player_name):
-    number_to_guess = random.randint(1, 100)
+    
+    clear_screen()
+    
     attempts = 0
 
-    print(f"Привет, {player_name}! Я загадал число от 1 до 100. Попробуй угадать его!")
+    print(f"Привет, {player_name}! Я загадал число от 1 до 100. Попробуй угадать его!\nУ тебя {max_attempts} попыток.\n")
 
-    while True:
-        guess = int(input("Введите ваше предположение: "))
+    while attempts < max_attempts:
+       
+        remaining_attempts = max_attempts - attempts
+        
+        while True:
+           try:
+            guess = int(input("Введите ваше предположение: "))
+            break  # Прерываем цикл, если введено целое число
+           except ValueError:
+            print("Введите число. Попробуйте снова.")
+
         attempts += 1
 
         if guess < number_to_guess:
-            print("Загаданное число БОЛЬШЕ вашего..")
+            clear_screen()
+            print(f"\n!Подасказка. Загаданное число БОЛЬШЕ {guess}. (Оставшиеся попытки: {remaining_attempts})")
         elif guess > number_to_guess:
-            print("Загаданное число МЕНЬШЕ вашего..")
+            clear_screen()
+            print(f"\n!Подасказка. Загаданное число МЕНЬШЕ {guess}. (Оставшиеся попытки: {remaining_attempts})")
         else:
-            print(f"Поздравляю, {player_name}! Вы угадали число {number_to_guess} за {attempts} попыток.")
+            clear_screen()
+            print(f"\nПоздравляю, {player_name}! Вы угадали число {number_to_guess} за {attempts} попыток.")
             update_leaderboard(player_name, attempts)
             break
-game_menu()
+
+    if attempts == max_attempts:
+        print(f"\nУ вас закончились попытки. Загаданное число было: {number_to_guess}. Повезет в другой раз :)\n")
+
+try:
+  clear_screen()
+  game_menu()
+except KeyboardInterrupt:
+        print("\nВыход из программы.")
